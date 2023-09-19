c47e8b5d41b406f71ca0da586c0c407c11eac270
Rui | 罗尔 <66991791+qrovic@users.noreply.github.com>
2023-09-20 01:38:15 +0800

    remove toggle if mobile

--- sidebar.js
+++ sidebar.js
@@ -1,26 +1,41 @@
 const body = document.querySelector("body");
-const sidebar = body.querySelector(".sidebar");
-const toggle = body.querySelector(".toggle");
-const listItems = sidebar.querySelectorAll("ul"); // Select all <li> elements within the sidebar
+        const sidebar = body.querySelector(".sidebar");
+        const toggle = body.querySelector(".toggle");
+        const listItems = sidebar.querySelectorAll("ul"); // Select all <li> elements within the sidebar
 
-function toggleSidebar() {
-    sidebar.classList.toggle("close");
-    sidebar.classList.toggle("open");
-}
-
-function checkOverflow() {
-    listItems.forEach((item) => {
-        // Check if the text content overflows the container
-        if (item.scrollWidth > item.clientWidth) {
-            sidebar.classList.add("close"); // Close the sidebar
+        function toggleSidebar() {
+            sidebar.classList.toggle("close");
+            sidebar.classList.toggle("open");
         }
-    });
-}
 
-toggle.addEventListener("click", () => {
-    toggleSidebar();
-});
+        function checkOverflow() {
+            listItems.forEach((item) => {
+                // Check if the text content overflows the container
+                if (item.scrollWidth > item.clientWidth) {
+                    sidebar.classList.add("close"); // Close the sidebar
+                }
+            });
+        }
 
-// Check for overflow initially and on window resize
-window.addEventListener("resize", checkOverflow);
-window.addEventListener("load", checkOverflow);
+        function openSidebar() {
+            sidebar.classList.remove("close"); // Remove the "closed" class to open the sidebar
+        }
+
+        function disableOpenSidebar() {
+            toggle.style.display = "none"; // Hide the toggle button
+        }
+
+        // Detect if the device width is less than a certain breakpoint (e.g., 768px)
+        const isMobile = window.innerWidth < 768;
+
+        // Disable opening the sidebar on mobile devices
+        if (isMobile) {
+            disableOpenSidebar();
+        } else {
+            // Enable opening the sidebar when not on a mobile device
+            toggle.addEventListener("click", openSidebar);
+        }
+
+        // Check for overflow initially and on window resize
+        window.addEventListener("resize", checkOverflow);
+        window.addEventListener("load", checkOverflow);
